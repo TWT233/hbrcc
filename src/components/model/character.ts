784ca35be3df863ef71d91e83cbb24b8e0485234
TWT <twt2333@outlook.com>
2023-11-12 01:35:25 +0800

    enum

--- src/components/model/character.ts
+++ src/components/model/character.ts
@@ -1,18 +1,19 @@
-import {Skill} from "./skill";
+import {DefaultSkill, Skill} from "./skill";
+import {DefaultATKBuff} from "./modifier/modifier";
 
 export class Character {
-    stat: Record<Stat, number>
-    skill: Skill
+    stat: Record<Stat, number> = {CON: 0, DEX: 0, LUK: 0, SPR: 0, STR: 0, WIS: 0}
+    skill: Skill = new DefaultSkill()
 }
 
 export enum Stat {
-    STR,
-    DEX,
+    STR = "STR",
+    DEX = "DEX",
 
-    CON,
-    SPR,
+    CON = "CON",
+    SPR = "SPR",
 
-    WIS,
-    LUK,
+    WIS = "WIS",
+    LUK = "LUK",
 }
 

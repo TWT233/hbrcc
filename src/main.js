991ac20e0df9d4eae567bddf0d103678c4a250c5
TWT <twt2333@outlook.com>
2023-11-12 00:00:43 +0800

    new dep

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import 'vuetify/styles'
 import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
 
 const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+        defaultTheme: 'dark',
+    },
 })
 
 createApp(App).use(vuetify).mount('#app')

002e93c07723080a48af1ee8f4afa0423921768c
TWT <twt2333@outlook.com>
2023-11-12 01:01:32 +0800

    add selector enemy

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@ import {defineProps, ref} from "vue";
 
 import SelectorMod from "@/components/selector/SelectorMod.vue";
 import {Modifier} from "@/components/model/modifier/modifier";
+import SelectorEnemy from "@/components/selector/SelectorEnemy.vue";
+import {Enemy} from "@/components/model/enemy";
 
 const mods = ref<Modifier[]>([])
+const enemy = ref(new Enemy())
 
 </script>
 
@@ -15,6 +18,9 @@ const mods = ref<Modifier[]>([])
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
       <v-container>
         <v-row>
+          <v-col cols="12" lg="4">
+            <SelectorEnemy v-model:enemy="enemy"></SelectorEnemy>
+          </v-col>
           <v-col cols="12" md="6" lg="4">
             <SelectorMod v-model:selected-mods="mods"></SelectorMod>
           </v-col>

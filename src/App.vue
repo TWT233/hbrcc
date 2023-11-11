d48f6dc65d21d4dea814c32d5759a7bc55480402
TWT <twt2333@outlook.com>
2023-11-12 00:01:28 +0800

    ui

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
-<script setup>
+<script setup lang="ts">
+import {defineProps, ref} from "vue";
+
+import SelectorMod from "@/components/selector/SelectorMod.vue";
+import {Modifier} from "@/components/model/modifier/modifier";
+
+const mods = ref<Modifier[]>([])
+
 </script>
 
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar"></v-app-bar>
+    <v-app-bar :elevation="8" title="hbrcc"></v-app-bar>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      Main Content
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6" lg="4">
+            <SelectorMod v-model:selected-mods="mods"></SelectorMod>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-layout>
 </template>
---
+++ src/components/selector/SelectorMod.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import {defineProps, defineEmits} from "vue"
+
+import ShowMod from "@/components/selector/ShowDefaultMod.vue";
+
+import {Modifier, DefaultATKBuff} from "@/components/model/modifier/modifier";
+import {ATK} from "@/components/model/modifier/types";
+
+const props = defineProps<{
+  selectedMods: Modifier[]
+}>()
+
+const emit = defineEmits<{
+  update: [
+    selectedMods: Modifier[],
+  ]
+}>()
+
+function addMod() {
+  props.selectedMods.push(new DefaultATKBuff(ATK.NORMAL, 0.5))
+}
+
+function deleteMod(index) {
+  props.selectedMods.splice(index, 1)
+}
+</script>
+
+<template>
+  <v-card>
+    <v-toolbar>
+      <v-toolbar-title>Buffs</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon="mdi-plus" @click="addMod()"></v-btn>
+    </v-toolbar>
+    <v-container>
+      <v-row>
+        <v-col v-for="i in selectedMods.length" :key="i" cols="auto">
+          <ShowMod v-model:mod="selectedMods[i-1]" @del="deleteMod(i-1)"></ShowMod>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>

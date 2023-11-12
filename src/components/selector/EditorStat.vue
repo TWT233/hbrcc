39c1f9cd944525859de7c4d5b0945a4d5c9484be
chirowang <chirowang@tencent.com>
2023-11-12 12:23:28 +0800

    stat

---
+++ src/components/selector/EditorStat.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {defineEmits, defineProps} from "vue";
+
+import {Stat} from "@/components/model/character";
+
+const props = defineProps<{
+  stat: Record<Stat, number>,
+}>()
+
+const emit = defineEmits<{
+  update: [
+    stat: Record<Stat, number>,
+  ]
+}>()
+</script>
+
+<template>
+  <v-card elevation="8" variant="outlined" title="Stat">
+    <v-container>
+      <v-row>
+        <v-col v-for="i in Object.keys(Stat)" :key="i">
+          <v-text-field v-model="stat[i]" :label="i"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>

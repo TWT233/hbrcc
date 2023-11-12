e7cacb0934b69ebefd13bc2d5dc007a2a80d3b5e
TWT <twt2333@outlook.com>
2023-11-13 01:17:53 +0800

    arrange comp

--- src/App.vue
+++ src/App.vue
@@ -15,23 +15,17 @@ const hit = ref(new Hit())
   <v-layout class="rounded rounded-md">
     <v-app-bar :elevation="8" title="hbrcc"></v-app-bar>
 
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+    <v-main class="d-flex align-center justify-center">
       <v-container>
         <v-row>
           <v-col cols="12" lg="4">
-            <v-row>
-              <v-col>
-                <ViewChar v-model:char="hit.char"></ViewChar>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <ViewEnemy v-model:enemy="hit.enemy"></ViewEnemy>
-              </v-col>
-            </v-row>
+            <ViewChar v-model:char="hit.char"></ViewChar>
           </v-col>
           <v-col cols="12" lg="4">
             <ViewMods v-model:modMap="hit.modMap" title="Buffs & DeBuffs"></ViewMods>
+          </v-col>
+          <v-col cols="12" lg="4">
+            <ViewEnemy v-model:enemy="hit.enemy"></ViewEnemy>
           </v-col>
         </v-row>
       </v-container>

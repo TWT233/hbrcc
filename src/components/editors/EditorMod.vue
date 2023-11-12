59d52ce7bf87fd84564b89b364ee6dfbe0a79fef
TWT <twt2333@outlook.com>
2023-11-12 22:31:08 +0800

    vtabs

--- src/components/editors/EditorMod.vue
+++ src/components/editors/EditorMod.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts">
-import {computed, defineEmits, defineProps, ref} from "vue";
+<script lang="ts" setup>
+import {defineEmits, defineProps} from "vue";
 
 import {Modifier} from "@/components/model/modifier/modifier";
 import {MainTypes} from "@/components/model/modifier/types";
@@ -14,38 +14,17 @@ const emit = defineEmits<{
   ],
   del: []
 }>()
-
-const tab = ref(null)
 </script>
 
 <template>
-  <v-dialog activator="parent" width="auto">
-    <v-card>
-      <v-toolbar title="Buff Detail">
-        <v-btn icon="mdi-delete" @click="$emit('del')"></v-btn>
-      </v-toolbar>
-      <v-tabs
-          v-model="tab"
-          bg-color="primary"
-      >
-        <v-tab v-for="i in Object.keys(MainTypes)" :value="i">{{ i }}</v-tab>
-      </v-tabs>
-      <v-window v-model="tab">
-      </v-window>
-
-
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-text-field v-model="mod.sub" label="sub type"></v-text-field>
-          </v-col>
-          <v-col>
-            <v-text-field v-model="mod.value" label="value" type="number"></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-  </v-dialog>
+  <v-card>
+    <v-toolbar title="Buff Detail">
+      <v-btn icon="mdi-delete" @click="$emit('del')"></v-btn>
+    </v-toolbar>
+    <v-tabs v-model="mod.main">
+      <v-tab v-for="i in Object.keys(MainTypes)" :value="i">{{ i }}</v-tab>
+    </v-tabs>
+  </v-card>
 </template>
 
 <style scoped>

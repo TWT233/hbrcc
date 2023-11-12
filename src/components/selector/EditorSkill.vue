efc2dc22b593d502515b0117414a5c580c5ef1f4
chirowang <chirowang@tencent.com>
2023-11-12 12:23:24 +0800

    skill

---
+++ src/components/selector/EditorSkill.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {defineEmits, defineProps} from "vue";
+
+import {Skill} from "@/components/model/skill";
+
+
+const props = defineProps<{
+  skill: Skill,
+}>()
+
+const emit = defineEmits<{
+  update: [
+    skill: Skill,
+  ]
+}>()
+</script>
+
+<template>
+  <v-card elevation="8" variant="outlined" title="Skill">
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field v-model="skill.cap" label="Skill cap" type="number"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field v-model="skill.bar[0]" label="Min DMG" type="number"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field v-model="skill.bar[1]" label="Max DMG" type="number"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>

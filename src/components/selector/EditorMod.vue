b57432316271f62f621f42989a301b963da02b10
TWT <twt2333@outlook.com>
2023-11-12 20:40:14 +0800

    rename

--- src/components/selector/EditorMod.vue
+++ src/components/selector/EditorMod.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import {defineProps, defineEmits} from "vue"
+import {defineEmits, defineProps} from "vue";
 
 import {Modifier} from "@/components/model/modifier/modifier";
-import {COLOR_MAP} from "@/components/model/modifier/types";
 
 const props = defineProps<{
   mod: Modifier,
@@ -17,27 +16,23 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <v-chip closable @click:close="$emit('del')" :color="COLOR_MAP[mod.main]">
-    <v-icon start icon="mdi-pencil"></v-icon>
-    {{ mod.main }} | {{ mod.sub }} | {{ (mod.value * 100 | 0) }}%
-    <v-dialog activator="parent" width="auto">
-      <v-card>
-        <v-toolbar title="Buff Detail">
-          <v-btn icon="mdi-delete" @click="$emit('del')"></v-btn>
-        </v-toolbar>
-        <v-container>
-          <v-row>
-            <v-col>
-              <v-text-field v-model="mod.sub" label="sub type" type="number"></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field v-model="mod.value" label="value" type="number"></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-dialog>
-  </v-chip>
+  <v-dialog activator="parent" width="auto">
+    <v-card>
+      <v-toolbar title="Buff Detail">
+        <v-btn icon="mdi-delete" @click="$emit('del')"></v-btn>
+      </v-toolbar>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="mod.sub" label="sub type"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="mod.value" label="value" type="number"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-dialog>
 </template>
 
 <style scoped>

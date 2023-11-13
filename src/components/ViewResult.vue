94a11f2860a2d8115ec3fbc7513ff8327c69edde
chirowang <chirowang@tencent.com>
2023-11-13 14:45:03 +0800

    view res

---
+++ src/components/ViewResult.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {defineProps} from "vue"
+import {Hit} from "@/model/hit";
+
+const props = defineProps<{
+  hit: Hit,
+}>()
+
+const emit = defineEmits<{
+  'update:hit': [
+    hit: Hit
+  ]
+}>()
+</script>
+
+<template>
+  <v-card>
+    <v-toolbar title="Result"></v-toolbar>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card-title>Damage</v-card-title>
+          <v-card-text>{{ hit.calculate() }}</v-card-text>
+        </v-col>
+        <v-col>
+          <v-switch v-model="hit.isCrit" label="Critical" color="primary"></v-switch>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card-text>current mods:{{ hit.modMap }}</v-card-text>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>

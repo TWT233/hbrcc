5b1d692ac3af16e181b5bd1e4d52673071a8eb50
chirowang <chirowang@tencent.com>
2023-11-15 10:46:53 +0800

    hoju

--- src/model/effect.ts
+++ src/model/effect.ts
@@ -9,25 +9,16 @@ export class Effect {
 
     baseOn: Partial<StatMap>// eg: {[STR]: 2, [DEX]: 1} means 2*STR+1*DEX
 
-    produce(lv: number, stat: Stat, border: number): number {
+    produce(lv: number, hojuLV: number, stat: Stat, border: number): number {
         const bar = this.calcBar(lv)
         const sd = this.calcES(stat) - border
 
-        if (sd < -this.cap / 2) {
-            return 1
-        }
-        if (sd <= 0) {
-            return (sd + this.cap / 2) * bar[0] / (this.cap / 2)
-        }
-        if (sd < this.cap) {
-            return (bar[1] - bar[0]) * sd / this.cap
-        }
-        return bar[1]
+        return this.calcValue(bar, sd, this.cap) + this.calcHoju(bar, sd, hojuLV)
     }
 
     calcBar(lv: number): [number, number] {
-        const g: ArbitraryGrowth = (this.growth instanceof Array) ? this.growth : growthRateMap[this.growth]
-        return [this.bar[0] * (1 + g[0] * lv), this.bar[1] * (1 + g[1] * lv)]
+        const growth = (this.growth instanceof Array) ? this.growth : growthRateMap[this.growth]
+        return [this.bar[0] * (1 + growth[0] * lv), this.bar[1] * (1 + growth[1] * lv)]
     }
 
     calcES(stat: Stat): number {
@@ -38,6 +29,26 @@ export class Effect {
             smultiplier += this.baseOn[s]
         }
         return sall / smultiplier
+    }
+
+    calcValue(bar: [number, number], sd: number, cap: number) {
+        if (sd < -cap / 2) {
+            return 1
+        }
+        if (sd <= 0) {
+            return (sd + cap / 2) * bar[0] / (cap / 2)
+        }
+        if (sd < cap) {
+            return (bar[1] - bar[0]) * sd / cap
+        }
+        return bar[1]
+    }
+
+    calcHoju(bar: [number, number], sd: number, lv: number): number {
+        const growth = (this.hojuGrowth instanceof Array) ? this.hojuGrowth : hojuGrowthRateMap[this.hojuGrowth]
+        const hojuBar: [number, number] = [bar[0] * growth[0] * lv, bar[1] * growth[0] * lv]
+        const hojuCap = this.cap + growth[1] * lv
+        return this.calcValue(hojuBar, sd, hojuCap)
     }
 
 }

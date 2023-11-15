aef9ce4af2f90ac0c05d59be5f30d1659f96bb77
TWT <twt2333@outlook.com>
2023-11-15 23:53:30 +0800

    ViewNS.vue

---
+++ src/components/ViewNS.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+
+import {ref} from "vue";
+import {SkillName} from "@/data/skills";
+import {NS} from "@/model/effect";
+import {SkillData} from "@/data/localstorage";
+import EditorNS from "@/components/editors/EditorNS.vue";
+
+let val = ref<{
+  selected: SkillName | NS,
+  data: SkillData,
+}>({
+  selected: [],
+  data: {
+    lv: 1,
+    hoju: 0,
+    stat: {CON: 0, DEX: 320, LUK: 0, SPR: 0, STR: 320, WIS: 0},
+  },
+})
+
+</script>
+
+<template>
+  <EditorNS :border="280" v-model:selected="val.selected" v-model:data="val.data"></EditorNS>
+</template>
+
+<style scoped>
+
+</style>

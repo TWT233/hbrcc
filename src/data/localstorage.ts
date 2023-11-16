46b0ee3c76c7593c23b923d96dcf1b91ed27f22f
TWT <twt2333@outlook.com>
2023-11-16 23:05:45 +0800

    wrap skill data

--- src/data/localstorage.ts
+++ src/data/localstorage.ts
@@ -3,7 +3,14 @@ import {SkillName} from "@/data/skills";
 import {SkillParam} from "@/model/skill";
 
 function getSkillData(sn: SkillName): SkillParam | undefined {
-    return JSON.parse(localStorage.getItem(getSDKey(sn))) as SkillParam
+    const key = getSDKey(sn)
+    let res: SkillParam
+    try {
+        res = JSON.parse(localStorage.getItem(key)) as SkillParam
+    } catch (e) {
+        localStorage.setItem(key, '')
+    }
+    return res
 }
 
 function getSDKey(sn: SkillName): string {

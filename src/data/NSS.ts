883047a59a212aab7e3d52ffd6b41d32db32016a
chirowang <chirowang@tencent.com>
2023-11-16 10:46:46 +0800

    NSS

--- src/data/NSS.ts
+++ src/data/NSS.ts
@@ -2,41 +2,41 @@ import {NS} from "@/model/effect";
 import {ADDITIONAL_S, ATK_ELEMENT_S, ATK_NORMAL_L, CRIT_NORMAL, dmg, ENEMY_TYPE} from "@/data/effects";
 import {EnemyType} from "@/model/types";
 
-export enum SkillName {
+export enum NSName {
     AliceASkill51 = "天使光線",
     LShanhuaSkill51 = "第七計 無中生有",
     SHiguchiSkill52 = "絕對現象"
 }
 
-export const SKILLS: Record<SkillName, NS> = {
-    [SkillName.AliceASkill51]: [
+export const NSS: Record<NSName, NS> = {
+    [NSName.AliceASkill51]: [
         ATK_ELEMENT_S(147),
         ENEMY_TYPE(EnemyType.HP, 0.5),
         dmg([2502, 12510], 147, {STR: 2, DEX: 1}),
     ],
-    [SkillName.LShanhuaSkill51]: [
+    [NSName.LShanhuaSkill51]: [
         ATK_NORMAL_L(248, true),
         ADDITIONAL_S,
     ],
-    [SkillName.SHiguchiSkill52]: [
+    [NSName.SHiguchiSkill52]: [
         ATK_NORMAL_L(232, true),
         CRIT_NORMAL,
     ]
 }
 
-export const SKILLS_DMG = [
-    SkillName.AliceASkill51,
+export const NSS_DMG = [
+    NSName.AliceASkill51,
 ]
 
-export const SKILLS_MOD = {
+export const NSS_MOD = {
     FREQUENTLY: [
-        SkillName.LShanhuaSkill51,
-        SkillName.SHiguchiSkill52,
+        NSName.LShanhuaSkill51,
+        NSName.SHiguchiSkill52,
     ],
     ADDITIONAL: [
-        SkillName.LShanhuaSkill51,
+        NSName.LShanhuaSkill51,
     ],
     CRIT: [
-        SkillName.SHiguchiSkill52,
+        NSName.SHiguchiSkill52,
     ]
 }

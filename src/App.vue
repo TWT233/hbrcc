18afcc0f78a2df9112bdb8cd42113693e7495b41
TWT <twt2333@outlook.com>
2023-11-16 22:48:20 +0800

    add panel

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import ViewResult from "@/components/ViewResult.vue";
 
 import {Hit} from "@/model/hit";
 import ViewBuffs from "@/components/ViewBuffs.vue";
+import EditorSkillCall from "@/components/EditorSkillCall.vue";
 
 const hit = ref(new Hit())
 
@@ -20,22 +21,23 @@ provide('border', border)
     <v-main class="d-flex align-center justify-center">
       <v-container>
         <v-row>
+          <v-col>
+            <EditorSkillCall v-model:call="hit.atk" type="DMG"></EditorSkillCall>
+          </v-col>
           <v-col cols="12" lg="4">
             <ViewBuffs v-model:calls="hit.buffs"></ViewBuffs>
           </v-col>
           <v-col cols="12" lg="4">
             <v-row>
               <v-col>
+                <EditorEnemy v-model:enemy="hit.enemy"></EditorEnemy>
               </v-col>
             </v-row>
             <v-row>
               <v-col>
-                <EditorEnemy v-model:enemy="hit.enemy"></EditorEnemy>
+                <ViewResult v-model:hit="hit"></ViewResult>
               </v-col>
             </v-row>
-          </v-col>
-          <v-col cols="12" lg="4">
-            <ViewResult v-model:hit="hit"></ViewResult>
           </v-col>
         </v-row>
       </v-container>

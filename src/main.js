5abafebf06421baf70e72a6d9037e5359192e16e
TWT <TWT2333@outlook.com>
2024-03-03 13:42:53 +0800

    pinia

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import 'vuetify/styles'
 import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css'
+import {createPinia} from "pinia"; // Ensure you are using css-loader
 
+
+const app = createApp(App)
 
 const vuetify = createVuetify({
     components,
@@ -16,5 +19,9 @@ const vuetify = createVuetify({
         defaultTheme: 'dark',
     },
 })
+app.use(vuetify)
 
-createApp(App).use(vuetify).mount('#app')
+const pinia = createPinia()
+app.use(pinia)
+
+app.mount('#app')
